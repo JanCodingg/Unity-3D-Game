2df5847dd9a94c3f21acfc214c4cc6c9da229295
JanCodingg <janhubmann257@gmail.com>
2020-10-08 21:00:54 +0200

    Schießen

---
+++ 3D Project/Assets/Scripts/Gegner/GegnerScript.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class GegnerScript : MonoBehaviour
+{
+    public GameObject Patrone;
+    
+
+
+    
+
+    private void OnCollisonEnter(Collider other)
+    {
+        Debug.Log("Triggert");
+        if(other.gameObject == Patrone.gameObject)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+}
--- 3D Project/Assets/Scripts/Player/Hand-Gegenstände/Waffe/PatroneBewegen.cs
+++ 3D Project/Assets/Scripts/Player/Hand-Gegenstände/Waffe/PatroneBewegen.cs
@@ -1,12 +1,16 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Security.Cryptography;
 using UnityEngine;
 
 public class PatroneBewegen : MonoBehaviour
 {
+    public GameObject Player;
+    private int timeSinceBeginning = 0;
     private void Start()
     {
         transform.Rotate(transform.rotation.x + 90, transform.rotation.y, transform.rotation.z);
+        timeSinceBeginning = (int)Time.time;
     }
 
     // Update is called once per frame
@@ -14,5 +18,19 @@ public class PatroneBewegen : MonoBehaviour
     {
 
         transform.Translate(Vector3.up * Time.deltaTime * 15, Space.Self);
+
+        if (Time.time - timeSinceBeginning >= 20)
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject != Player)
+        {
+            Destroy(this.gameObject);
+        }
+        
     }
 }
